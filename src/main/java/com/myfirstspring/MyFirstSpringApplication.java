0bc2fa5e589a12bcc4508e87643131078f52f380
Will Warren <wbwarren@wbwarren@alum.mit.edu>
2015-12-04 15:16:48 -0500

    Initial Commit

---
+++ src/main/java/com/myfirstspring/MyFirstSpringApplication.java
@@ -0,0 +1,12 @@
+package com.myfirstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstSpringApplication.class, args);
+    }
+}
